22e56266b0db440205a06604e973a4821413eb1b
MatthewObirek <matthewobirek@gmail.com>
2023-02-10 12:59:54 -0800

    Added basic path finding ability

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 import requests
 import json
 import math
-import cv2
-import sys
+from queue import PriorityQueue
+from geopy.distance import geodesic
 
 # Function to calculate the total distance and change in elevation between two points
-def calculate_distance(point1, point2, api_key):
+def calculate_elevation_difference(point1, point2, api_key):
     # Get the elevation data for the two points
     elev1 = get_elevation(point1, api_key)
     elev2 = get_elevation(point2, api_key)
@@ -13,19 +13,13 @@ def calculate_distance(point1, point2, api_key):
     # Calculate the change in elevation
     elev_diff = abs(elev1 - elev2)
 
-    # Calculate the distance between the two points using the Haversine formula
-    lat1, lng1 = point1
-    lat2, lng2 = point2
-    lat1, lat2 = map(math.radians, [lat1, lat2])
-    lng1, lng2 = map(math.radians, [lng1, lng2])
-    dlat = lat2 - lat1
-    dlng = lng2 - lng1
-    a = (math.sin(dlat/2)**2) + math.cos(lat1) * math.cos(lat2) * (math.sin(dlng/2)**2)
-    c = 2 * math.asin(math.sqrt(a))
-    distance = 6371 * c
+    return elev_diff
 
+def calculate_distance(point1, point2):
+    # Calculate the distance between the two points using the Haversine formula
+    distance = geodesic(point1, point2).kilometers
     # Return the total distance and change in elevation
-    return distance, elev_diff
+    return distance
 
 # Function to get the elevation for a given point from the Google Elevation API
 def get_elevation(point, api_key):
@@ -38,76 +32,123 @@ def get_elevation(point, api_key):
 # Function to find the shortest path between two points with an allowable change in altitude
 def find_shortest_path(start, end, api_key, max_elev_diff):
     # Add the starting point to the queue
-    queue = [(start, get_elevation(start, api_key))]
+    queue = PriorityQueue()
+    queue.put(( start, [start]))
     visited = set()
+    lat_diff_threshold = 0.005
+    lng_diff_threshold = 0.005
+    shortest_distance = calculate_distance(start, end)
 
     # Continue searching until the queue is empty
-    while queue:
+    while not queue.empty():
         # Get the next point from the queue
-        point, elev_diff = queue.pop(0)
-
+        point, path = queue.get()
+        nextPoint = None;
         # Check if the point has been visited
         if point in visited:
             continue
         visited.add(point)
 
-        # Check if the point is the end point
-        if point == end:
-            return elev_diff
+        # Check if the point is close enough to the end point
+        lat_end, lng_end = end
+        lat_pnt, lng_pnt = point
+        lat_diff = abs(lat_pnt - lat_end)
+        lng_diff = abs(lng_pnt - lng_end)
+        if lat_diff <= lat_diff_threshold and lng_diff <= lng_diff_threshold:
+            return True, path
 
         # Get the neighbors of the current point
         neighbors = get_neighbors(point)
 
         # Add the neighbors to the queue if the change in elevation is within the allowed limit
+        i = 0
+        chosen = 0
         for neighbor in neighbors:
-            distance, elev_diff = calculate_distance(point, neighbor, api_key)
-            if elev_diff <= max_elev_diff:
-                print(elev_diff)
-                queue.append((neighbor, elev_diff))
+            elev_change = calculate_elevation_difference(point, neighbor, api_key)
+            distance = calculate_distance(neighbor, end)
+            if elev_change <= max_elev_diff and distance < shortest_distance:
+                chosen = i
+                nextPoint = neighbor
+                shortest_distance = distance
+                print(shortest_distance)
+            i+=1
+
+
+        if nextPoint == None:
+            return False, path
+        
+        new_path = path + [nextPoint]
+        queue.put((neighbors[chosen], new_path))
+        
+        
 
     # Return None if no path was found
-    return None
+    return False, path
 
 # Function to get the neighbors of a given point
 def get_neighbors(point):
     # Add code to get the neighbors of the given point based on the desired resolution and geography.
     # Example:
     lat, lng = point
-    return [(lat + 0.025, lng + 0.025), (lat - 0.025, lng - 0.025), (lat + 0.025, lng - 0.025), (lat - 0.025, lng + 0.025)]
+    return [(lat + 0.005, lng),         (lat - 0.005, lng),
+            (lat, lng - 0.005),         (lat, lng + 0.005),
+            (lat + 0.005, lng + 0.005), (lat - 0.005, lng - 0.005),
+            (lat + 0.005, lng - 0.005), (lat - 0.005, lng + 0.005)]
 
-def display_route_on_map(api_key, center, pathstring):
+def display_route_on_map(api_key, center, pathstring, zoom):
 
     # Build the URL for the static map
     url = "https://maps.googleapis.com/maps/api/staticmap?"
-    zoom = 15
     scale = 4
 
     r = requests.get(url + "center=" + str(center[0])+","+str(center[1]) + "&zoom=" + str(zoom) 
                      + "&maptype=satellite" + "&scale=" + str(scale)
-                     + "&path="+ str(center[0]-.0025)+","+str(center[1]-.0025)+"|"+ str(center[0])+","+str(center[1])+"|"+ str(center[0]+.0025)+","+str(center[1])
+                     + "&path="+pathstring
                      + "&size=1200x1200&key=" + api_key)
     map = r.content
 
-    f = open("Pictures/Pictures"+str(center)+".png", 'wb')
+    f = open("Pictures/Pictures1"+str(center)+".png", 'wb')
     # r.content gives content, in this case gives image
     f.write(map)
     # close method of file object save and close the file
     f.close()
 
+def toURLString(pathSet):
+    path_string = ""
+    i = 0;
+    for point in pathSet:
+        if i != 0:
+            path_string += f"|"
+        print(i)
+        lat, lng = point
+        path_string += f"{lat},{lng}"
+        i=i+1
+    return path_string
 
 
 
 # Example usage
 start = (49.886894, -119.497638)
-end = (49.919958, -119.393355)
+end   = (49.919958, -119.393355)
 f = open("API_KEY.txt","r")
 api_key = f.readline()
 print(str(start[0])+","+str(start[1]))
 print(get_elevation(end, api_key))
 max_elev_diff = 100
-display_route_on_map(api_key, start)
-display_route_on_map(api_key, end)
 
-#shortest_path = find_shortest_path(start, end, api_key, max_elev_diff)
+distance = calculate_distance(start, end)
+#print(distance)
+#for i in range(1,20):
+#    lat, lng = start
+#    start = (lat-.00, lng+.005)
+#    distance = calculate_distance(start, end)
+#    print(distance)
+completePath, shortest_path = find_shortest_path(start, end, api_key, max_elev_diff)
+print("Complete Path: " + str(completePath))
+pathString = toURLString(shortest_path)
+print(pathString)
+display_route_on_map(api_key, start, pathString, 15)
+display_route_on_map(api_key, end, pathString, 15)
+display_route_on_map(api_key, (49.92689400000002,-119.44763800000004), pathString, 12)
 #print("Shortest path:", shortest_path)
 
